7de643acea4b267cd882e1eb679db94cb7a4d52e
constempus <constempus@gmail.com>
2023-04-10 00:28:54 +0900

    news1

--- src/konacrawler/modules/news1.py
+++ src/konacrawler/modules/news1.py
@@ -4,31 +4,31 @@ import parsel
 import aiohttp
 
 @kcc.register_module
-class KnCT1Crawler(kcc.KNCRModule):
+class WikitreeCrawler(kcc.KNCRModule):
     @staticmethod
     def info()->kcc.ModuleInfo:
         return {
-            "name":"언론사 일반 템플릿 1",
+            "name":"news1뉴스",
             "scope":[
-                "weekly.chosun.com",
-                "news.unn.net",
-                "www.dtnews24.com",
+                "www.news1.kr"
             ]
         }
     
     async def crawl(self, url: str) -> str:
         headers={"USER-AGENT":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
+        
         async with aiohttp.ClientSession(headers=headers) as session:
             async with session.get(url) as resp:
                 html = await resp.text()
+
         sele=parsel.Selector(html)
-        text_p = sele.css('#article-view-content-div>p')
+        text_p = sele.css('#articles_detail')
         text="\n".join(["".join(i.xpath(".//text()").extract()) for i in text_p])
-        return text
+        return text.strip().replace('\n\n', '\n')
 
 if __name__ == "__main__":
     import asyncio
-    url="http://weekly.chosun.com/news/articleView.html?idxno=25448"
-    cl=KnCT1Crawler()
+    url="https://www.news1.kr/articles/5009148"
+    cl=WikitreeCrawler()
     
     print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
