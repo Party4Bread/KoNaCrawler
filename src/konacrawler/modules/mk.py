11571a5a22a26ec909a52ec8485de6c38074cdad
party4bread <party4bread@gmail.com>
2023-04-10 00:28:39 +0900

    add mk

---
+++ src/konacrawler/modules/mk.py
@@ -0,0 +1,34 @@
+from typing import TypeVar, TypedDict
+import konacrawler.core as kcc
+import parsel
+import aiohttp
+import lxml
+
+@kcc.register_module
+class MKCrawler(kcc.KNCRModule):
+    @staticmethod
+    def info()->kcc.ModuleInfo:
+        return {
+            "name":"매일경제",
+            "scope":[
+                "www.mk.co.kr"
+            ]
+        }
+    
+    async def crawl(self, url: str) -> str:
+        headers={"USER-AGENT":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.get(url) as resp:
+                html = await resp.text()
+
+        sele = parsel.Selector(html)
+        text_p = sele.css('.news_cnt_detail_wrap > p')
+        text="\n".join(["".join(i.xpath(".//text()").extract()) for i in text_p])
+        return text.strip()
+
+if __name__ == "__main__":
+    import asyncio
+    url="https://www.mk.co.kr/news/economy/10700859"
+    cl=MKCrawler()
+    
+    print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
