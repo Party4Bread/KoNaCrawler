1a89c7a31ee6ff16a7dfbf575dd12d85407666a4
constempus <constempus@gmail.com>
2023-04-10 22:50:17 +0900

    digital times

---
+++ src/konacrawler/modules/ajunews copy 2.py
@@ -0,0 +1,29 @@
+from typing import TypeVar, TypedDict
+import konacrawler.core as kcc
+from konacrawler.modules.knabs1 import Knabs1Crawler
+import parsel
+import aiohttp
+import lxml
+
+@kcc.register_module
+class AjuNewsCrawler(Knabs1Crawler):
+    rm_sel='.imgBox, div[class^="dcamp_ad"], .relate_box, .article_bot, .article_bot ~ *, script'
+    br_nl=False
+    p_nl=False
+    cont_sel='#articleBody'
+
+    @staticmethod
+    def info()->kcc.ModuleInfo:
+        return {
+            "name":"아주경제",
+            "scope":[
+                "www.ajunews.com"
+            ]
+        }
+
+if __name__ == "__main__":
+    import asyncio
+    url="https://www.ajunews.com/view/20210420133114713"
+    cl=AjuNewsCrawler()
+    
+    print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
---
+++ src/konacrawler/modules/digitaltimes.py
@@ -0,0 +1,29 @@
+from typing import TypeVar, TypedDict
+import konacrawler.core as kcc
+from konacrawler.modules.knabs1 import Knabs1Crawler
+import parsel
+import aiohttp
+import lxml
+
+@kcc.register_module
+class AjuNewsCrawler(Knabs1Crawler):
+    rm_sel='div:has(> img), iframe, center, .article_view>div'
+    br_nl=False
+    p_nl=False
+    cont_sel='.article_view'
+
+    @staticmethod
+    def info()->kcc.ModuleInfo:
+        return {
+            "name":"디지털타임즈",
+            "scope":[
+                "www.dt.co.kr"
+            ]
+        }
+
+if __name__ == "__main__":
+    import asyncio
+    url="http://www.dt.co.kr/contents.html?article_no=2023021602109919036008&ref=naver"
+    cl=AjuNewsCrawler()
+    
+    print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
