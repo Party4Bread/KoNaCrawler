aeec42d5cbccd8cc9faea65f73ae0d9a5ed71e79
constempus <constempus@gmail.com>
2023-04-10 23:31:27 +0900

    kpenews

--- src/konacrawler/modules/kpenews.py
+++ src/konacrawler/modules/kpenews.py
@@ -7,23 +7,23 @@ import lxml
 
 @kcc.register_module
 class JoongdoCrawler(Knabs1Crawler):
-    rm_sel='#font > table, p'
+    rm_sel='.cont-area>div'
     br_nl=True
     p_nl=False
-    cont_sel='#font'
+    cont_sel='.cont-area'
 
     @staticmethod
     def info()->kcc.ModuleInfo:
         return {
-            "name":"중도일보",
+            "name":"한국정경신문",
             "scope":[
-                "www.joongdo.co.kr"
+                "kpenews.com"
             ]
         }
 
 if __name__ == "__main__":
     import asyncio
-    url="http://www.joongdo.co.kr/web/view.php?key=20230406010001797"
+    url="http://kpenews.com/View.aspx?No=53810"
     cl=JoongdoCrawler()
     
     print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
