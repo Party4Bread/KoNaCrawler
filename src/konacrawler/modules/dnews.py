9da77bb3f6ad768c9c486ce244e94234f480979b
constempus <constempus@gmail.com>
2023-04-11 01:55:01 +0900

    dnews

--- src/konacrawler/modules/dnews.py
+++ src/konacrawler/modules/dnews.py
@@ -6,24 +6,24 @@ import aiohttp
 import lxml
 
 @kcc.register_module
-class EknCrawler(Knabs1Crawler):
-    rm_sel='table'
+class DnewsCrawler(Knabs1Crawler):
+    rm_sel='table, p>strong'
     br_nl=True
     p_nl=False
-    cont_sel='#news_body_area_contents'
+    cont_sel='.text'
 
     @staticmethod
     def info()->kcc.ModuleInfo:
         return {
-            "name":"에너지경제",
+            "name":"대한경제",
             "scope":[
-                "www.ekn.kr"
+                "www.dnews.co.kr"
             ]
         }
 
 if __name__ == "__main__":
     import asyncio
-    url="https://www.ekn.kr/web/view.php?key=20230404010000813"
-    cl=EknCrawler()
+    url="https://www.dnews.co.kr/uhtml/view.jsp?idxno=202304050909092580934"
+    cl=DnewsCrawler()
     
     print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
