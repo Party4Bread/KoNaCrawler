54a646d713b0fdc72ff56bb83928efe871649bd3
constempus <constempus@gmail.com>
2023-04-11 02:05:30 +0900

    bravo

--- src/konacrawler/modules/bravo.py
+++ src/konacrawler/modules/bravo.py
@@ -1,21 +1,25 @@
 from typing import TypeVar, TypedDict
 import konacrawler.core as kcc
+from konacrawler.modules.knabs1 import Knabs1Crawler
 import parsel
 import aiohttp
 import lxml
 
 @kcc.register_module
-class HaniCrawler(kcc.KNCRModule):
+class BravoCrawler(Knabs1Crawler):
+    # rm_sel='.articleView>div'
+    # br_nl=True
+    # p_nl=True
+    # cont_sel='.articleView'
+
     @staticmethod
     def info()->kcc.ModuleInfo:
         return {
-            "name":"한겨레",
+            "name":"브라보",
             "scope":[
-                "h21.hani.co.kr",
-                "www.hani.co.kr",
+                "bravo.etoday.co.kr"
             ]
         }
-    
     async def crawl(self, url: str) -> str:
         headers={"USER-AGENT":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
         
@@ -23,18 +27,14 @@ class HaniCrawler(kcc.KNCRModule):
             async with session.get(url) as resp:
                 html = await resp.text()
 
-        doc=lxml.html.fromstring(html)
-
-        # for bad in doc.cssselect('#ct > p > span'):
-        #     bad.getparent().remove(bad)
-        
-        ele=doc.cssselect("#contents > div > p")
-        text='\n'.join(i.text_content() for i in ele)
+        sele=parsel.Selector(html)
+        text_p = sele.css('.articleView > p')
+        text="\n".join(["\n".join(i.xpath(".//text()").extract()) for i in text_p])
         return text.strip()
 
 if __name__ == "__main__":
     import asyncio
-    url="https://h21.hani.co.kr/arti/society/society_general/50242.html"
-    cl=HaniCrawler()
+    url="https://bravo.etoday.co.kr/view/atc_view/12834"
+    cl=BravoCrawler()
     
     print(asyncio.get_event_loop().run_until_complete(cl.crawl(url)))
